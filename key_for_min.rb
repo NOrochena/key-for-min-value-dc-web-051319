05c511eb0cdf49d2ba89482a6365e039a2835def
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-05-02 15:35:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
     if lowest == [] 
       lowest = [key, value]
     elsif lowest[1] > value
-      lowest[1] = value
+      lowest = [key, value]
     end
   end
   lowest[0]
